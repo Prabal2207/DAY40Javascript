fc67b46b0b1cfc13a7f89a25b4fbad8e3980b103
Prabal2207 <prabal760@gmail.com>
2021-08-20 07:52:58 +0530

    [Prabal] Added:- Selection problems

---
+++ Selection/ArithmaticCal.js
@@ -0,0 +1,24 @@
+function CalculationOne(valueOne,valueTwo,valueThree)
+{
+    let ResultOne=valueOne+valueTwo*valueThree;
+    console.log(ResultOne);
+}
+function CalculationTwo(valuOne,valuTwo,valuThree)
+{
+    let resultTwo=valuThree+(valuOne/valuTwo);
+    console.log(resultTwo);
+}
+function CalculationThree(valueOne,valueTwo,valueThree)
+{
+    let resultThree=(valueOne%valueTwo)+valueThree;
+    console.log(resultThree);
+}
+function CalculationFour(valueOne,valueTwo,valueThree)
+{
+    let resultFour=(valueOne*valueTwo)+valueThree;
+    console.log(resultFour);
+}
+CalculationOne(2,2,1);
+CalculationTwo(2,4,1);
+CalculationThree(6,2,2)
+CalculationFour(2,2,1)
