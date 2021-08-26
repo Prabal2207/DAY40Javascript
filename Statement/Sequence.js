b980cf03eabbb9a0f4cc540737b82de94cf73521
Prabal2207 <prabal760@gmail.com>
2021-08-26 07:21:36 +0530

    [Prabal] Added:-Sequence problem

---
+++ Statement/Sequence.js
@@ -0,0 +1,15 @@
+// Sequence Practice Problems
+
+// Question No. 1
+console.log(Math.floor(Math.random() * 10));
+
+// Question No. 2
+console.log(Math.floor(Math.random() * 6) + 1);
+
+// Question No. 3
+let firstDiceNumber = Math.floor(Math.random() * 6) + 1;
+console.log("-> First Random Dice Number is = " +firstDiceNumber);
+let secondDiceNumber = Math.floor(Math.random() * 6) + 1;
+console.log("-> Second Random Dice Number is = " +secondDiceNumber);
+let sum = firstDiceNumber + secondDiceNumber;
+console.log("Addition of two Randon Dice Numbers => " + (sum));
